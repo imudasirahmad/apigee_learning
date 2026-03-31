023dcee0fda03b8c6b71da1a4040ecf19b4b8d66
MudasirAK <mudasirahmad0311@gmail.com>
2026-03-31 15:28:28 +0500

    added improvement to Get all users and developed  get user by id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,7 +34,23 @@ router.get("/users", async (req, res) => {
     }
 
     const users = await user.find(query);
+    if(users.length === 0) {
+      return res.status(404).json({ error: "No users found" });
+    }
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+req.get("/users:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userData = await user.findById(id);
+    if (!userData) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(userData);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
