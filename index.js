1b4104192f5f707e088ee27219c3bc1b55d243fb
MudasirAK <mudasirahmad0311@gmail.com>
2026-04-15 14:34:14 +0500

    port changed

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/api-learning")
+mongoose.connect(process.env.MONGO_URI,)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log("MongoDB connection error:", err));
 
 app.use("/api", userRoutes);
 
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 }); 
